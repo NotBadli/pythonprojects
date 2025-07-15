515fa6d40d21e1b1b9e91383dcdb1dda8ba05799
NotBadli <79380248+NotBadli@users.noreply.github.com>
2025-07-15 17:11:23 +0300

    Update optionsLC 1.17.1.py

--- optionsLC 1.17.1.py
+++ optionsLC 1.17.1.py
@@ -1,6 +1,6 @@
 import time, zipfile
 
-ZIP_DIRECTORY = "C:/Users/illia/Downloads/Options/optionsLC 1.17.1"
+ZIP_DIRECTORY = "C:/Users/notbadli/Downloads/Options/optionsLC 1.17.1"
 ZIP_DESTINATION = "C:/Users/illia/AppData/Roaming/.minecraft"
 
 with zipfile.ZipFile(ZIP_DIRECTORY, 'r') as zip_ref:
